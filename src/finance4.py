dacd62bc30dea7516ef507bd4baed33e3c88cb24
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-05 14:17:37 -0400

    added processLabelData function

--- src/finance4.py
+++ src/finance4.py
@@ -4,3 +4,18 @@ pd.core.common.is_list_like = pd.api.types.is_list_like
 import matplotlib.pyplot as plt
 from matplotlib import style
 import pickle
+
+def process_data_for_labels(ticker):
+    hm_days = 7
+    df = pd.read_csv('sp500_joined_closes.csv', index_col=0)
+    tickers = df.columns.values
+    df.fillna(0, inplace=True)
+
+    for i in range(1, hm_days+1):
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]        ##financial formula
+    
+    df.fillna(0, inplace=True)
+    return tickers,df
+
+
+process_data_for_labels('GOOG')
