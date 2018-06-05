14471d6078d25794cbd399fcb67728a1ea4f8a99
Davit Julakidze <Davit.Julakidze@structuretone.com>
2018-06-05 16:42:31 -0400

    fixes

--- src/finance4.py
+++ src/finance4.py
@@ -43,7 +43,6 @@ def get_data_from_yahoo(reload_sp500=False):
     start = dt.datetime(2010, 1, 1)
     end = dt.datetime.now()
     for ticker in tickers:
-        # just in case your connection breaks, we'd like to save our progress!
         if not os.path.exists('stock_dfs/{}.csv'.format(ticker)):
             df = web.DataReader(ticker, 'morningstar', start, end)
             df.reset_index(inplace=True)
